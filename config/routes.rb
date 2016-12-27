65c8b908a54a995f664cc94b52283fcd62b9aa1b
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2016-12-27 17:02:21 +0900

    /userへのアクセスが可能に

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :signup, only: [:new, :create]
   root to: 'signup#new'
 
-  resources :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
